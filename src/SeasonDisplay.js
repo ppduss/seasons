be89b84baf57e2349f5da3f1763ea1d2bb91ddeb
Patrick <ppduss@gmail.com>
2020-01-28 13:08:45 -0600

    Update

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 const seasonConfig = {
     summer: {
-        text: "Let's hot the beach!",
+        text: "Let's hit the beach!",
         iconName: "sun"
     },
     winter: {
